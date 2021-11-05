e7b428325fa9e474acafca7c1f4724ea72c7b23f
Ben Lovy <ben@deciduously.com>
2021-11-05 18:43:41 -0400

    Move python reqs to flake, fmt main.rs

--- languages/python/build/main.rs
+++ languages/python/build/main.rs
@@ -7,9 +7,9 @@ struct Args {
 	/// The directory to run the build and output the wheel.  MUST BE EMPTY/NONEXISTENT
 	#[clap(short, long)]
 	build_path: PathBuf,
-    /// The .pyi interface file
-    #[clap(short, long)]
-    interface_path: PathBuf,
+	/// The .pyi interface file
+	#[clap(short, long)]
+	interface_path: PathBuf,
 	/// The shared object library (.so, .dylib, .dll)
 	#[clap(short, long)]
 	lib_path: PathBuf,
@@ -20,7 +20,12 @@ struct Args {
 
 fn main() {
 	let args = Args::parse();
-	let paths = Paths::new(args.interface_path, args.lib_path, args.metadata_path, args.build_path);
+	let paths = Paths::new(
+		args.interface_path,
+		args.lib_path,
+		args.metadata_path,
+		args.build_path,
+	);
 	if let Err(e) = build_wheel(paths) {
 		eprintln!("{}", e);
 		std::process::exit(1);
