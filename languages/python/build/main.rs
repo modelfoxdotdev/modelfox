f918f5b5e21d874f8b1081283517661592c328fa
Ben Lovy <ben@deciduously.com>
2021-11-08 18:30:04 -0500

    Update build_wheel

--- languages/python/build/main.rs
+++ languages/python/build/main.rs
@@ -1,6 +1,6 @@
-use build_wheel::{build_wheel, Paths};
+use build_wheel::{build_wheel, Config, Tag};
 use clap::Parser;
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
 #[derive(Parser)]
 struct Args {
@@ -9,22 +9,26 @@ struct Args {
 	build_path: PathBuf,
 	/// The .pyi interface file
 	#[clap(short, long)]
-	interface_path: PathBuf,
+	interface_path: Option<PathBuf>,
 	/// The shared object library (.so, .dylib, .dll)
 	#[clap(short, long)]
 	lib_path: PathBuf,
 	/// The location of the TOML file defining the metadata.
 	#[clap(short, long)]
 	metadata_path: PathBuf,
+    /// The tag to build for.  Default is "cp36-abi3-manylinux_2_24_x86_64".
+    tag: Option<String>
 }
 
 fn main() {
 	let args = Args::parse();
-	let paths = Paths::new(
+    let tag = args.tag.map(|s| Tag::from_str(&s).expect("Could not parse tag"));
+	let paths = Config::new(
 		args.interface_path,
 		args.lib_path,
 		args.metadata_path,
 		args.build_path,
+        tag
 	);
 	if let Err(e) = build_wheel(paths) {
 		eprintln!("{}", e);
