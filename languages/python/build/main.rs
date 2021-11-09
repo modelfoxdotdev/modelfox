750439589d71c0656e539f0fef20b94b3908b348
Ben Lovy <ben@deciduously.com>
2021-11-09 10:17:33 -0500

    Refactor wheel builder

--- languages/python/build/main.rs
+++ languages/python/build/main.rs
@@ -1,12 +1,13 @@
-use build_wheel::{build_wheel, Config, Tag};
+//! This example demonstrates how to use `build_wheel` via a small command-line tool.
+use build_wheel::WheelBuilder;
 use clap::Parser;
-use std::{path::PathBuf, str::FromStr};
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct Args {
 	/// The directory to run the build and output the wheel.  MUST BE EMPTY/NONEXISTENT
 	#[clap(short, long)]
-	build_path: PathBuf,
+	build_path: Option<PathBuf>,
 	/// The .pyi interface file
 	#[clap(short, long)]
 	interface_path: Option<PathBuf>,
@@ -16,21 +17,23 @@ struct Args {
 	/// The location of the TOML file defining the metadata.
 	#[clap(short, long)]
 	metadata_path: PathBuf,
-    /// The tag to build for.  Default is "cp36-abi3-manylinux_2_24_x86_64".
-    tag: Option<String>
+	/// The tag to build for.  Default is "cp36-abi3-manylinux_2_24_x86_64".
+	tag: Option<String>,
 }
 
 fn main() {
 	let args = Args::parse();
-    let tag = args.tag.map(|s| Tag::from_str(&s).expect("Could not parse tag"));
-	let paths = Config::new(
-		args.interface_path,
-		args.lib_path,
-		args.metadata_path,
-		args.build_path,
-        tag
-	);
-	if let Err(e) = build_wheel(paths) {
+	let mut builder = WheelBuilder::new(args.lib_path, args.metadata_path);
+	if let Some(p) = args.interface_path {
+		builder = builder.type_interface(p);
+	}
+	if let Some(p) = args.build_path {
+		builder = builder.output_dir(p);
+	}
+	if let Some(t) = args.tag {
+		builder = builder.tag(&t).expect("Could not parse tag");
+	}
+	if let Err(e) = builder.build() {
 		eprintln!("{}", e);
 		std::process::exit(1);
 	}
