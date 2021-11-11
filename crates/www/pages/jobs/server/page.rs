c8702c72864b3acd6f31fc841cd659515f4a1ae1
Isabella Tromba <isabella.tromba@gmail.com>
2021-11-10 21:57:29 -0500

    update job description

--- crates/www/pages/jobs/server/page.rs
+++ crates/www/pages/jobs/server/page.rs
@@ -7,13 +7,10 @@ pub struct Page;
 impl Component for Page {
 	fn into_node(self) -> Node {
 		let title = ui::H1::new().child("Jobs");
-		let job1 = ui::S2::new().child(ui::H2::new().child("Rust Developer")).child(ui::P::new().child("Do you love Rust, Machine Learning, and writing really fast and memory efficient code? At Tangram, you'll get to work on everything from our core machine learning algorithms to writing front-end code in Rust! We are looking for developers with experience in Rust and familiarity with (or willingness to learn) machine learning concepts. If this sounds exciting, let's talk! Email us at ").child(ui::Link::new().href("mailto:jobs@tangram.dev".to_string()).title("jobs@tangram.dev".to_string()).child("jobs@tangram.dev")));
-		let job2 = ui::S2::new()
-			.child(ui::H2::new().child("Machine Learning Developer Advocate"))
-			.child(ui::P::new().child("Do you love writing blog posts about machine learning and developer tools? Do you want to get on calls with developers and demo Tangram and figure out how to help them be successful deploying machine learning models? If this sounds exciting, let's talk! Email us at ").child(ui::Link::new().href("mailto:jobs@tangram.dev".to_string()).title("jobs@tangram.dev".to_string()).child("jobs@tangram.dev")));
+		let job1 = ui::S2::new().child(ui::H2::new().child("Rust Developer (Remote)")).child(ui::P::new().child(ui::Markdown::new("Tangram makes it easy for programmers to train, deploy, and monitor machine learning models. With Tangram, developers can train models and make predictions on the command line or with libraries for languages including Elixir, Go, JS, PHP, Python, Ruby, and Rust, and learn about their models and monitor them in production from a web application. Watch the demo on the [homepage](https://www.tangram.dev). Tangram is open source and everything is written in Rust, from the core machine learning algorithms to the web application. Check it out on [GitHub](https://www.github.com/tangramdotdev/tangram). We are looking for programmers who love developer tools and are excited to build machine learning tools with Rust! If you are interested, email us at [jobs@tangram.dev](mailto:jobs@tangram.dev).".into())));
 		let content = div()
 			.style("display", "grid")
-			.child(ui::S1::new().child(title).child(job1).child(job2));
+			.child(ui::S1::new().child(title).child(job1));
 		Document::new()
 			.child(PageLayout::new().child(content))
 			.into_node()
