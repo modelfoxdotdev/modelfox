5bb160b9b6431ea210d623bbf2f68ec1befd3589
Ben Lovy <ben@deciduously.com>
2022-01-13 15:12:44 -0500

    fix lint

--- crates/app/core/lib.rs
+++ crates/app/core/lib.rs
@@ -231,8 +231,7 @@ impl AppState {
 /// Remove all contents of the data dir, including the database
 pub async fn reset_data(database_url: &Option<Url>) -> Result<()> {
 	if let Some(database_url) = database_url {
-		match database_url.scheme() {
-			"postgres" => {
+		if database_url.scheme() == "postgres" {
 				let pool = PgPoolOptions::new()
 					.max_connections(1)
 					.connect(database_url.as_str())
@@ -247,8 +246,7 @@ pub async fn reset_data(database_url: &Option<Url>) -> Result<()> {
 				sqlx::query("GRANT ALL ON SCHEMA public TO public")
 					.execute(&pool)
 					.await?;
-			}
-			_ => { /* No action for any other schema */ }
+			
 		}
 	}
 	std::fs::remove_dir_all(data_path()?)?;
