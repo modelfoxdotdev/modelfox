b826d249377a0c2b3770d5a8791f8f6d437aca3a
Ben Lovy <ben@deciduously.com>
2022-02-08 12:20:27 -0500

    pool to one

--- crates/app/core/test_common.rs
+++ crates/app/core/test_common.rs
@@ -24,7 +24,7 @@ pub fn init_test_options() -> crate::options::Options {
 	// set in-memory SQLite DB
 	let database_url = "sqlite::memory:".parse().expect("Malformed URL");
 	let database_options = crate::options::DatabaseOptions {
-		max_connections: None,
+		max_connections: Some(1),
 		url: database_url,
 	};
 	options.database = database_options;
