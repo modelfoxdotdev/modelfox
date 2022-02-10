48d15077a9c6fec22e116642de0c69f4ebe4227c
Ben Lovy <ben@deciduously.com>
2022-02-10 15:38:13 -0500

    Remove alert testing pending resolution of #97

--- crates/app/core/test_common.rs
+++ crates/app/core/test_common.rs
@@ -10,8 +10,7 @@ use crate::{
 use anyhow::{bail, Result};
 use chrono::{Datelike, TimeZone, Timelike, Utc};
 use num::ToPrimitive;
-use sqlx::PgPool;
-use std::{collections::HashMap, path::PathBuf, str::FromStr};
+use std::{collections::HashMap, path::PathBuf};
 use tangram::ClassificationOutputValue;
 use tangram_app_monitor_event::{
 	BinaryClassificationPredictOutput, MonitorEvent, NumberOrString, PredictOutput,
@@ -19,83 +18,8 @@ use tangram_app_monitor_event::{
 };
 use tangram_id::Id;
 use tangram_table::TableView;
-use url::Url;
 
-const TEST_POSTGRES_URL: &str = "postgres://postgres:test@0.0.0.0:5432";
-
-/// The TestContext handles establishing a PostgreSQL database to use for an individual test.
-///
-/// Use at the beginning of any test requiring a connection, and call drop when done:
-/// ```no_run
-/// #[tokio::test]
-/// async fn cool_test {
-/// 	let mut ctx = TestContext::new("cool_test").await;
-/// 	// test logic!
-/// 	let app = init_test_app(init_test_options_postgres("cool_test")).await.unwrap();
-/// 	// ...
-/// 	ctx.drop().await;
-/// }
-/// ```
-pub struct TestContext {
-	db_name: String,
-}
-
-impl TestContext {
-	pub async fn new(db_name: &str) -> Self {
-		// Connect to DB
-		let postgres_url = Url::from_str(&format!("{}/postgres", TEST_POSTGRES_URL)).unwrap();
-		// Create database for test
-		let pool = PgPool::connect(postgres_url.as_str())
-			.await
-			.expect("Failed to connect to test database");
-		// First, clear the deck in case of previous fail
-		let query_str = format!("DROP DATABASE IF EXISTS {}", db_name);
-		sqlx::query(&query_str)
-			.execute(&pool)
-			.await
-			.expect("Could not clean up existing DB");
-		// Create new DB
-		let query_str = format!("CREATE DATABASE {}", db_name);
-		sqlx::query(&query_str)
-			.execute(&pool)
-			.await
-			.expect("Could not create test database");
-		Self {
-			db_name: db_name.to_owned(),
-		}
-	}
-
-	pub async fn drop(&mut self) {
-		// Connect to database
-		let postgres_url = Url::from_str(&format!("{}/postgres", TEST_POSTGRES_URL)).unwrap();
-		let pool = PgPool::connect(postgres_url.as_str())
-			.await
-			.expect("Failed to connect to test database");
-		// Forcibly disconnect active users
-		sqlx::query(
-			"
-			select
-				pg_terminate_backend(pid)
-			from
-				pg_stat_activity
-			where
-				datname = $1
-		",
-		)
-		.bind(&self.db_name)
-		.execute(&pool)
-		.await
-		.expect("Unable to unceremoniously dump users");
-		// Drop database
-		let query_str = format!("DROP DATABASE {}", self.db_name);
-		sqlx::query(&query_str)
-			.execute(&pool)
-			.await
-			.expect("Could not drop test database");
-	}
-}
-
-pub fn init_test_options_sqlite() -> crate::options::Options {
+pub fn init_test_options() -> crate::options::Options {
 	let mut options = crate::options::Options::default();
 	// set in-memory SQLite DB
 	let database_url = "sqlite::memory:".parse().expect("Malformed URL");
@@ -109,28 +33,12 @@ pub fn init_test_options_sqlite() -> crate::options::Options {
 	options
 }
 
-pub fn init_test_options_postgres(db_name: &str) -> crate::options::Options {
-	let mut options = crate::options::Options::default();
-	// set in-memory SQLite DB
-	let database_url = format!("{}/{}", TEST_POSTGRES_URL, db_name)
-		.parse()
-		.expect("Malformed URL");
-	let database_options = crate::options::DatabaseOptions {
-		max_connections: None,
-		url: database_url,
-	};
-	options.database = database_options;
-	// Use in-memory storage
-	options.storage = crate::options::StorageOptions::InMemory;
-	options
-}
-
-pub async fn init_test_app(options: crate::options::Options) -> Result<App> {
-	let app = App::new(options).await?;
+pub async fn init_test_app() -> Result<App> {
+	let app = App::new(init_test_options()).await?;
 	Ok(app)
 }
 
-/// This module is nsested several layers deep, but the resources are relative to the workspace root
+/// This module is nested several layers deep, but the resources are relative to the workspace root
 pub fn workspace_root() -> PathBuf {
 	let crate_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 	crate_root
