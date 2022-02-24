054f8763a2a61f21dd4d386a5ddcd00ecc496188
Ben Lovy <ben@deciduously.com>
2022-02-24 13:38:20 -0500

    Cleanup unnecessary as_str() calls

--- languages/javascript/node/build.rs
+++ languages/javascript/node/build.rs
@@ -1,6 +1,6 @@
 fn main() {
 	let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-	if target_os.as_str() == "macos" {
+	if target_os == "macos" {
 		println!("cargo:rustc-cdylib-link-arg=-undefined");
 		println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
 	}
--- languages/python/build.rs
+++ languages/python/build.rs
@@ -1,10 +1,10 @@
 fn main() {
 	let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-	if target_os.as_str() == "macos" {
+	if target_os == "macos" {
 		println!("cargo:rustc-cdylib-link-arg=-undefined");
 		println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
 	}
-	if target_os.as_str() == "windows" {
+	if target_os == "windows" {
 		println!(
 			"cargo:rustc-link-search=native={}",
 			std::env::var("CARGO_MANIFEST_DIR").unwrap()
